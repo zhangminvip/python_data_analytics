8742a2119617cf8f537ec334a01ac69a71d5fc68
minzhang <a991391009@gmail.com>
2019-01-15 19:01:37 +0800

    add bsm call value function

--- finance/c5_com/BSM_option_valuation.py
+++ finance/c5_com/BSM_option_valuation.py
@@ -21,4 +21,8 @@ def N(d):
     return quad(lambda x: dN(x), -20, d, limit=50)[0]
 
 
-
+def BSM_call_value(St, K, t, T, r, sigma):
+    d1 = d1f(St, K, t, T, r, sigma)
+    d2 = d1 - sigma * math.sqrt(T - t)
+    call_value = St * N(d1) - math.exp(-r * (T - t)) * K * N(d2)
+    return call_value
