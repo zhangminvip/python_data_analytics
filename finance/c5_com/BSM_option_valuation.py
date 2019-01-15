df4713551bfb1c1c5087edbd46cbcd5c3a6677e3
minzhang <a991391009@gmail.com>
2019-01-15 19:00:27 +0800

    add d1f function

--- finance/c5_com/BSM_option_valuation.py
+++ finance/c5_com/BSM_option_valuation.py
@@ -7,20 +7,16 @@ from scipy.integrate import quad
 mpl.rcParams['font.family'] = 'serif'
 
 
-def dlf(St, K, t, T, r, sigma):
+def d1f(St, K, t, T, r, sigma):
     d1 = (math.log(St / K) + (r + 0.5 * sigma ** 2)
           * (T - t)) / (sigma * math.sqrt(T - t))
+    return d1
 
 
-def dN(x):
-    return math.exp(-0.5 * x ** 2) / math.sqrt(2 * math.pi)
 
 
-def N(d):
-    return quad(lambda x: dN(x), -20, d, limit=50)[0]
 
 
-def BSM_call_value(St, K, t, T, r, sigma):
-    d1 = dlf(St, K, t, T, r, sigma)
-    d2 = d1 - sigma * math.sqrt(T - t)
-    call_value = St * N(d1) - math.exp(-r * (T - t)) * K * N(d2)
+
+
+
